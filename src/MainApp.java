5cd78b0248ea6df8f207c0bca21cb05e2df2d411
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-04 08:49:23 -0300

    First commit -numbers double

---
+++ src/MainApp.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        double x = 10.554;
+        int y = 5;
+        double soma = x + y;
+
+        System.out.printf("%.2f%n", soma);
+
+        Locale.setDefault(Locale.US);
+
+
+
+
+    }
+}
