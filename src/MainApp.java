d6fbdc1327dd619a9b6e50b3d30c305685281de2
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-05 14:05:44 -0300

    homework conditional structure

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,17 +5,22 @@ public class MainApp {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int s1;
+        int time;
 
-        s1 = sc.nextInt();
+        time = sc.nextInt();
 
 
-          if (s1 < 12) {           // if you find this, stop, otherwise go to the next one until you find the real one
+          if (time < 12) {           // if you find this, stop, otherwise go to the next one until you find the real one
              System.out.println("GOOD MORNING");
-        } else if ( s1 < 18) {
+        } else if ( time < 18) {
              System.out.println("GOOD AFTERNOON");
         } else {
-             System.out.println("GOOD NIGHT");
+             if (time < 24) {
+                 System.out.println("GOOD NIGHT");
+             }
+             else {
+                 System.out.println("THIS IS NOT A HOUR");
+             }
         }
 
         sc.close();
