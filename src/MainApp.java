871d4def3ce2504308c0ebda1eea5634f381f377
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-04 23:58:01 -0300

    Casting

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,35 +3,18 @@ import java.util.Locale;
 public class MainApp {
     public static void main(String[] args) {
 
-       String product1 = "Computer";
-       String product2 = "Office desk";
+       int x = 5;
+       int y = 2;
+       double soma  = (double) x / y;
 
-       int age = 31;
-       int code = 5250;
-       char gender = ('F');
+       System.out.println(soma);
 
-       double price1 = 2100.0;
-       double price2 = 650.50;
-       double measure = 53.234567;
+
+
 
     //specification
 
-        System.out.printf(
-                "products: %n%s, which price is $ %.2f %n%s, which price is $ %.2f%n%n",
-                 product1,price1,product2,price2
-        );
 
-        System.out.printf(
-                "Record: %d years old, code %d and gender:%c%n%n",
-                 age,code,gender
-        );
-        System.out.printf(
-                "Measure with eight decimal places: %.8f %nRounded(three decimal places): %.3f%nUS decimal point: %s%n",
-                 measure,
-                 measure,
-                 String.format(Locale.US,"%.3f", measure)
-        );
-        Locale.setDefault(Locale.US);
 
 
 
