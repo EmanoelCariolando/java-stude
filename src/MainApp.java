7d0fb7d02142ee535f79cf6bbd2697803e6be245
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-05 23:18:16 -0300

    ternary conditional

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,42 +3,14 @@ import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
         Scanner sc = new Scanner(System.in);
-        int x;
-        double value = 50.0;
 
-        x = sc.nextInt();
+        double value = sc.nextDouble();
 
-        switch (x){
-            case 1:
-            System.out.println("It's Sunday");
-            break;
-            case 2:
-                System.out.println("It's Monday");
-                break;
-            case 3:
-                System.out.println("It's Tuesday");
-                break;
-            case 4:
-                System.out.println("It's Wednesday");
-                break;
-            case 5:
-                System.out.println("It's Thursday");
-                break;
-            case 6:
-                System.out.println("It's Friday");
-                break;
-            case 7:
-                System.out.println("It's Saturday");
-                break;
-
-                default:
-                System.out.println("this day does not exist");
-                break;
-        }
-
-
+        double promotion = (value < 20.0) ? value - 0.5 : value - 1.5;
+        System.out.println(promotion);
 
 
         sc.close();
