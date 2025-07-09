5a3da9cc0f467d67c38d216eb8882d84f2003eb1
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-07-08 21:43:01 -0300

    Interface - Interface Comparable
    Replaces usage of device-related entity classes in MainApp with file reading and sorting logic. Removes ComboDevice, Device, Printer, Scanner, SolidPrinter, and SolidScanner classes from the model.entities package, simplifying the codebase and removing unused device abstractions.

--- src/Application/MainApp.java
+++ src/Application/MainApp.java
@@ -1,18 +1,36 @@
 package Application;
 
-
-
-import model.entities.ComboDevice;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class MainApp {
     public static void main(String[] args) throws ParseException {
 
-        ComboDevice c = new ComboDevice("2010");
-        System.out.println(c.scan() + "Bag");
-        c.processDoc("Bodys");
-        c.print("Things");
-     }
+        List<String> list = new ArrayList<>();
+        String path = "C:\\out\\Re.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String lines = br.readLine();
+            while (lines != null){
+               list.add(lines);
+               lines = br.readLine();
+            }
+            Collections.sort(list);
+            for(String s :list){
+                System.out.println(s);
+            }
+
+        }
+
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     }
--- src/model/entities/Scanner.java
+++
@@ -1,5 +0,0 @@
-package model.entities;
-
-public interface Scanner  {
-    public String scan();
-}
