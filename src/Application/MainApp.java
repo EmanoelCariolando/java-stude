a8d65039f3c7e2842701b1e5f9bc08196883d9bd
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-08-08 21:58:01 -0300

    Refactor to use Client class and remove shape entities
    Removed Circle, Retangle, and Shape classes and their usage from MainApp. Introduced a new Client class with equals and hashCode implementations. MainApp now demonstrates Client equality instead of shape area calculations.

--- src/Application/MainApp.java
+++ src/Application/MainApp.java
@@ -1,11 +1,8 @@
 package Application;
-import entities.Circle;
-import entities.Retangle;
-import entities.Shape;
+
+import entities.Client;
 
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,21 +11,16 @@ public class MainApp {
     public static void main(String[] args) throws ParseException {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+        Client c1 = new Client("Mary","Mamao@gmail.com");
+        Client c2 = new Client("Mary","Mamao@gmail.com");
 
-        List<Shape> myShapes = new ArrayList<>();
-        myShapes.add(new Circle(2.0));
-        myShapes.add(new Retangle(2.0,3.0));
+        System.out.println(c1.equals(c2));
 
-        List<Circle> myCircles = new ArrayList<>();
-        myCircles.add(new Circle(1.0));
-
-        System.out.println("total area: " + totalArea(myShapes));
-    }
-    public static double totalArea(List<? extends Shape> l){
-        double sum = 0.0;
-        for (Shape item: l){
-          sum+= item.area();
+        if (c1.equals(c2)){
+            System.out.println("Já Existe");
         }
-        return sum;
+        else {
+            System.out.println("vtmncccc");
+        }
     }
     }
